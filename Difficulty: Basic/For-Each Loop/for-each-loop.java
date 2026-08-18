2e6517e84068b76cb2db36a0cdc0256c8e59260b
RETIKKA K M <retikkakm1808@gmail.com>
2026-08-18 12:11:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For-Each Loop/for-each-loop.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void printArray(String[] arr) {
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+        
+    }
+}
