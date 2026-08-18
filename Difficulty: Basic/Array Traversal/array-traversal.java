ffac8291dcf1c26f2fca9f55f4001c7614f8352e
RETIKKA K M <retikkakm1808@gmail.com>
2026-08-18 12:10:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Array Traversal/array-traversal.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static void arrayTraversal(int[] arr) {
+        int i=0;
+        for(i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    
+        
+    }
+}
