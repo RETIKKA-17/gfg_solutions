0bbcc893317ec1eb25186b7e365975659af91317
RETIKKA K M <retikkakm1808@gmail.com>
2026-09-07 22:30:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Multiplication Table/multiplication-table.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=10;i++)
+        {
+            int j=i*n;
+            System.out.print(j+" ");
+        }
+        
+        
+    }
+}
