1026791688c899eb188c8232050d99179badaade
RETIKKA K M <retikkakm1808@gmail.com>
2026-09-08 22:46:28 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Prime Number/prime-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    static boolean isPrime(int n) {
+        int i,count=0;
+        for(i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+}
