f143dbebfb2ba74fdd8b75847cf18dfa0948d751
RETIKKA K M <retikkakm1808@gmail.com>
2026-09-08 20:54:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/GCD of Two/gcd-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int gcd(int a, int b) {
+        int rem;
+        while(b!=0)
+        {
+            rem=a%b;
+            a=b;
+            b=rem;
+        }
+        return a;
+    }
+}
